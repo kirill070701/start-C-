24f6c8b316905214bf9da6e9e75fbf620ebeecec
Kirill Andreev <kirill07.07.01@mail.ru>
2020-04-02 16:34:11 +0300

    nachalo

---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("введите число");
+            int two, three;
+            string one, output;
+            one = Console.ReadLine();
+            two = Convert.ToInt32(one);
+            three = two + 6;
+            output = Convert.ToString(three);
+            Console.WriteLine(output);
+
+        }
+    }
+}
